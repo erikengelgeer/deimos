1afd183c95251788ac987b23cdd733501d0135f4
Nena.Leeuwenburg <eotwi@agfahealthcare.com>
2016-08-26 14:47:20 +0200

    profile angular functions

--- app/Resources/assets/js/components/profile/ProfileController.js
+++ app/Resources/assets/js/components/profile/ProfileController.js
@@ -5,27 +5,16 @@ function ProfileController($rootScope) {
     $rootScope.loading = false;
 
     vm.measurePassword = measurePassword;
-//    TODO: code for password strength
+    vm.save = save;
+
     vm.password = "";
     vm.confirmationPassword = "";
+    vm.strengthText = '';
 
     var progressBar = $(".progress-bar");
-    var progressText = $(".progress-text");
     var strength = 0;
     var colors = ["#eb0000", "#FD662E", "#EBBA00", "#68c721", "#00b050"];
     var strengths = ["Very Weak", "Weak", "Better", "Strong", "Very Strong"];
-
-//    TODO: revise the code and make changes if needed (convert it to angular)
-/*    $("#save").click(function () {
-        if(passwordInput.val() != '') {
-            var result = zxcvbn(passwordInput.val());
-
-            if(result.score == 0 || result.score == 1) {
-                var message = "are you sure? your password is " + strengths[result.score].toLowerCase() + ", \nyou might want to change your password. \nThe given password can be guessed within " + Math.ceil(result.crack_times_seconds.online_throttling_100_per_hour / 86400) + " day(s).";
-                alert(message);
-            }
-        }
-    })*/
 
     function measurePassword() {
         console.log(vm.password);
@@ -36,16 +25,38 @@ function ProfileController($rootScope) {
 
         if (vm.password == '') {
             strength = 0;
-            progressBar.find("span").html(null);
         }
 
         progressBar.css("width", strength + "%");
         progressBar.css("background-color", colors[result.score]);
-        progressText.html("<em>" + strengths[result.score] + "</em>");
+        vm.strengthText = strengths[result.score];
 
         if (vm.password == '') {
-            strength = 0;
-            progressText.html(null);
+            vm.strengthText = '';
+        }
+    }
+
+    function save() {
+        // Error if not entered the password.
+        if(vm.password == '' && vm.confirmationPassword == '' ) {
+            var errorRePassword = "You haven't enter the password and retype password";
+            console.log(errorRePassword);
+        }
+
+
+        if(vm.password.trim() != vm.confirmationPassword.trim()) {
+        //    error
+            return;
+        }
+
+        if(vm.password.trim() != '') {
+            var result = zxcvbn(vm.password);
+
+            if(result.score == 0 || result.score == 1) {
+            //    Do something
+                var message = "are you sure? your password is " + strengths[result.score].toLowerCase() + ", \nyou might want to change your password. \nThe given password can be guessed within " + Math.ceil(result.crack_times_seconds.online_throttling_100_per_hour / 86400) + " day(s).";
+                console.log(message);
+            }
         }
     }
 }
