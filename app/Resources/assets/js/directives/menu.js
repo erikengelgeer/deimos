1e560d2f4c457b571482c9ccbef8996c3d57572d
Nick Keereweer <nick.keereweer@agfa.com>
2016-09-13 14:11:03 +0200

    changed the way menu shows. fixed datetimepicker problem

--- app/Resources/assets/js/directives/menu.js
+++ app/Resources/assets/js/directives/menu.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('menu',menu);
+angular.module('app').directive('menu', menu);
 
 function menu() {
 
@@ -7,8 +7,29 @@ function menu() {
         restrict: "A",
         templateUrl: "partials/nav.html",
         scope: {},
-        controller: function ($scope, $rootScope) {
+        controllerAs: 'vm',
+        controller: function ($scope, $rootScope, $state, $timeout) {
+            var vm = this;
 
+            vm.checkStates = checkStates;
+
+
+            function checkStates() {
+                var states = [  'manage-shifts', 'manage-shifts-edit', 'manage-shifts-new',
+                                'manage-tasks', 'manage-tasks-edit', 'manage-tasks-new',
+                                'manage-users', 'manage-users-edit', 'manage-users-new',
+                                'manage-teams', 'manage-teams-edit', 'manage-teams-new',
+                                'plan-users', 'plan-tasks'];
+                
+                for(var i = 0; i < states.length; i++) {
+                    result = $state.current.name == states[i];
+
+                    if(result) {
+                        break;
+                    }
+                }
+                return result;
+            }
         }
     }
 
