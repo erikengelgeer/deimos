707a0b9b76ba8e6181f47999d722d4012fef9fd8
Nick Keereweer <nick.keereweer@agfa.com>
2016-09-20 08:42:38 +0200

    created an array of dates between monday and sunday 3 weeks later

--- src/AppBundle/Controller/Api/ShiftsController.php
+++ src/AppBundle/Controller/Api/ShiftsController.php
@@ -22,17 +22,75 @@ class ShiftsController extends Controller
      */
     public function findAllAction()
     {
+
         $em = $this->getDoctrine()->getManager();
         $repository = $em->getRepository("AppBundle:Shift");
 
-        $now = new \DateTime();
-        $timestamp = strtotime('+4 weeks');
+//        $now = new \DateTime();
+        $timestamp = strtotime('sunday this week +3 weeks');
         $fourWeeksLater = date_timestamp_set(new \DateTime(), $timestamp);
+
+        $timestamp2 = strtotime('monday this week');
+        $mondayThisWeek = date_timestamp_set(new \DateTime(), $timestamp2);
+
+        $dates = [];
+
+//        $interval = $mondayThisWeek->diff($fourWeeksLater);
+//        $intervalDays = $interval->format("%d days");
+//        settype($intervalDays, "int");
+
+        $diff = $mondayThisWeek->diff($fourWeeksLater);
+        $totalDays = intval($diff->format('%d'));
+//        dump($totalDays);
+
+        $weekArray = array();
+        $planningArray = array();
+        $currentTimestamp = $mondayThisWeek->getTimestamp();
+        $dayCount = 1;
+//        for($i = 0; $i < ($totalDays + 1); $i++) {
+//
+//
+//            if($dayCount <= 7) {
+//                $date = new \DateTime();
+//                $date->setTimestamp($currentTimestamp);
+//                array_push($weekArray, $date);
+//
+//            } else {
+//                dump("new week");
+//                array_push($planningArray, $weekArray);
+//                $weekArray = array();
+//                $dayCount = 0;
+//            }
+//
+////
+////            $test = new \DateTime();
+////            $test->setTimestamp($currentTimestamp);
+////            dump($test);
+////           dump(new \DateTime($currentTimestamp));
+////            array_push($testArray, $currentTimestamp);
+//            $dayCount++;
+//            $currentTimestamp += 86400;
+//        }
+
+
+        for ($i = 0; $i <= 3; $i++) {
+            for ($j = 0; $j <= 6; $j++) {
+                $date = new \DateTime();
+                $date->setTimestamp($currentTimestamp);
+                array_push($weekArray, $date);
+                $currentTimestamp += 86400;
+            }
+
+            array_push($planningArray, $weekArray);
+            $weekArray = array();
+        }
+        dump($planningArray);
+
 
         /**
          * @var ShiftRepository $repository
          */
-//        $shifts = $repository->findAllAction($now, $fourWeeksLater);
+//        $shifts = $repository->findAllAction($mondayThisWeek, $fourWeeksLater);
         $shifts = $repository->findAll();
 
         $data = $this->get('serializer')->serialize($shifts, 'json');
@@ -47,8 +105,9 @@ class ShiftsController extends Controller
      */
     public function findOneByAction(Shift $shift)
     {
-        $data= $this->get('serializer')->serialize($shift, 'json');
+        $data = $this->get('serializer')->serialize($shift, 'json');
         return new Response($data, 200, ['Content-type' => 'application/json']);
     }
+
 
 }
