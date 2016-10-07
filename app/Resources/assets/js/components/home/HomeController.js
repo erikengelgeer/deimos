158c499e97925fbf3512a489395e5afb36da6763
Nick Keereweer <nick.keereweer@agfa.com>
2016-10-07 10:49:22 +0200

    you can now change the team on the weekly planner (not the best way)

--- app/Resources/assets/js/components/home/HomeController.js
+++ app/Resources/assets/js/components/home/HomeController.js
@@ -1,6 +1,6 @@
 angular.module('app').controller('HomeController', HomeController);
 
-function HomeController($rootScope, Api, $q) {
+function HomeController($rootScope, Api) {
     var vm = this;
 
     vm.users = [];
@@ -14,28 +14,43 @@ function HomeController($rootScope, Api, $q) {
 
 
     console.log($rootScope.loading);
-    $rootScope.loading = false;
-    console.log($rootScope.loading);
+    // $rootScope.loading = false;
+    // console.log($rootScope.loading);
 
 
     Api.getTeams().then(function (response){
         vm.teams = response.data;
         console.log(vm.teams);
 
+        //TODO: Team is hardcoded, needs to be read from logged in user.
         vm.selectedTeam = vm.teams[0];
         loadShiftsByTeam(vm.selectedTeam.id);
     });
 
-
     function selectTeam() {
-        console.log(vm.selectedTeam.id)
+        // console.log(vm.selectedTeam.id);
+        refreshData(vm.selectedTeam.id);
     }
+
+    function refreshData(teamId){
+
+        loadShiftsByTeam(teamId);
+
+    }
+
     function loadShiftsByTeam(teamId) {
+        vm.users = [];
+        vm.planning = [];
+        vm.planningUsers = [];
+        vm.shifts = [];
         // console.log(teamId);
         Api.getAllShifts(teamId).then(function (response) {
             vm.shifts = response.data;
 
             buildSchedule(teamId);
+            buildScheduleCss();
+            $rootScope.loading = false;
+            console.log($rootScope.loading);
 
         });
     }
@@ -133,5 +148,89 @@ function HomeController($rootScope, Api, $q) {
         return data;
     }
 
-    
+    function buildScheduleCss() {
+        setTimeout(function () {
+
+
+
+            // NOTE: Call this code after building the DOM, otherwise it won't work
+            // TODO: disable heading scrolling when working on other browsers than chrome and firefox (for now)
+
+            //@formatter:off
+            // Variables
+            // Items
+            var ScheduleContainer = $('.schedule-container'),
+                schedules = $('.schedule');
+
+            // Measures
+            var columns = 8,
+                scheduleContainerWidth = 'inherit',
+                scheduleContainerHeight = $(window).height() - 95;
+//            scheduleContainerHeight = 317;
+
+            var current = 1,
+                item = $('#' + current),
+                lastScrollTop = 0;
+            //@formatter:on
+
+            ScheduleContainer.css({'height': scheduleContainerHeight, 'width': scheduleContainerWidth});
+            $(window).resize(function () {
+                ScheduleContainer.css({'height': scheduleContainerHeight, 'width': scheduleContainerWidth});
+            });
+
+            buildSchedule();
+
+            function buildSchedule() {
+                var itemWidth = (100 / columns) + '%';
+                var heading = $('.heading');
+                var bodyRow = $('.body-row');
+
+                heading.find('.item').css({width: itemWidth});
+                heading.find('.item:last-child').css({width: itemWidth + 1});
+
+                bodyRow.find('.item').css({width: itemWidth});
+                bodyRow.find('.item:last-child').css({width: itemWidth + 1});
+            }
+
+            item.addClass('active');
+
+            ScheduleContainer.scroll(function (e) {
+                var currentScrollTop = $(this).scrollTop();
+                var nextItem = (current == schedules.length) ? $('#' + (current)) : $('#' + (current + 1));
+                var previousItem = (current == 1) ? $('#' + (current)) : $('#' + (current - 1));
+
+                if (currentScrollTop > lastScrollTop) {
+                    // DOWN
+                    console.log('scroll', $(this).scrollTop(), 'nextItem', (nextItem.offset().top - ScheduleContainer.offset().top));
+
+                    if ((nextItem.offset().top - ScheduleContainer.offset().top) <= 0 && current != schedules.length) {
+                        current++;
+
+                        item = $('#' + current);
+                        schedules.removeClass('active');
+                        item.addClass('active');
+                    }
+
+                } else {
+                    // UP
+                    console.log('scroll', $(this).scrollTop(), 'previousItem', (previousItem.offset().top - ScheduleContainer.offset().top));
+
+                    if ((previousItem.offset().top - ScheduleContainer.offset().top) >= (0 - (item.height() + item.find('.heading').height())) && current != 1) {
+                        current--;
+
+                        item = $('#' + current);
+                        schedules.removeClass('active');
+                        item.addClass('active');
+                    }
+
+                }
+                item.find('.heading').css('top', ($(this).scrollTop()));
+                lastScrollTop = currentScrollTop;
+            });
+        },4000);
+    }
+
+    function deleteSchedule() {
+
+    }
 }
