3b0b3048c05e07d25fc27b979134157cca8eab2c
Alexander Eijgermans <alexander,eijgermans@agfa.com>
2016-10-10 15:10:40 +0200

    restricted the management

--- app/Resources/assets/js/components/management/ManagementController.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').controller('ManagementController', ManagementController);
-
-function ManagementController($rootScope) {
-    var vm = this;
-    $rootScope.loading = false;
-
-}
